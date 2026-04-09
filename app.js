cb096566535260939642f8e35e3a85cae1bb01f9
ub-victor <victoireushindi371@gmail.com>
2026-04-09 03:29:59 +0200

    Initial commit

--- app.js
+++ app.js
@@ -20,6 +20,9 @@ const port = process.env.PORT || 3000;
 const start = async()=>{
     try{
         await connectDB(process.env.MONGO_URL)
+        app.listen(port, ()=>{
+            console.log(`Server is Running on port ${port}...`);
+        })
 
     }catch{
 
