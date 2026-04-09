abb456dcebfd4d9a8b64c5a8dfaa1905f52365b3
ub-victor <victoireushindi371@gmail.com>
2026-04-09 15:27:22 +0200

    Intinial commit

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,2 +1,17 @@
 // controller actions
 
+const signup_get = (req, res)=> {
+    res.render('signup')
+}
+
+const login_get = (req, res)=> {
+    res.render('signup')
+}
+
+const signup_post = (req, res)=> {
+    res.render('signup')
+}
+
+const signup_post = (req, res)=> {
+    res.render('signup')
+}
