ba9bd23cb4849efbac26873bce27d46f63713061
ub-victor <victoireushindi371@gmail.com>
2026-04-10 13:54:00 +0200

    Intinial commit

--- models/User.js
+++ models/User.js
@@ -1,12 +1,13 @@
 const mongoose = require('mongoose');
-const {isEmail} = require('validator');
+const {isEmail} = require('validator');// Import the isEmail function from the validator library to validate email addresses
 
 const userSchema = new mongoose.Schema({
     email:{
         type: String,
         required: [true, 'Please enter an email'],
         unique: true,
-        lowercase: true
+        lowercase: true,
+        validate: [isEmail, 'Please enter a valid email'] // Use the isEmail function from the validator library to validate the email format
     },
     password: {
         type: String,
