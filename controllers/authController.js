aed358e4beedf4b677108670bc10674fa4243499
ub-victor <victoireushindi371@gmail.com>
2026-04-10 12:37:29 +0200

    Intinial commit

--- controllers/authController.js
+++ controllers/authController.js
@@ -13,6 +13,7 @@ const signup_post = async (req, res)=> {
     const {email, password} = req.body;
     try {
         const user =  User.create({email, password});
+        res.status(200).json(user);
 
     }
     catch(error){
