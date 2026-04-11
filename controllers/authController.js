e5ef93410db4086c87e7e0d064808e386b7acc6a
ub-victor <victoireushindi371@gmail.com>
2026-04-11 23:42:41 +0200

    Logic setting up

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,7 +4,7 @@ const User = require('../models/User');
 
 // handle errors
 const handleErrors = (err)=>{
-    console.log(err.message, err.code)
+    console.log(err.message, err.code)// this log out the error msg and the code the error gave
     let errors = {email: '', password: ''};
 
     // duplicate email error
